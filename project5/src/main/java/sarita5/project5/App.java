3d2a3cb7e252f5efda958aed92eb090da12b61ec
Aarav <Aarav@LAPTOP-4BQN4NRF>
2022-12-12 21:06:52 -0500

    commit 1

---
+++ project5/src/main/java/sarita5/project5/App.java
@@ -0,0 +1,13 @@
+package sarita5.project5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
